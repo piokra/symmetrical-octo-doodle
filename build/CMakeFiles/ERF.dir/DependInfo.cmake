4074888e9641dbd2e388f9edc3c3c610cb599e3c
Piotr Krasun <piotr.krasun@gmail.com>
2018-02-10 13:52:47 -0500

    Another CMake change

---
+++ build/CMakeFiles/ERF.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/touchstone/ERF/symmetrical-octo-doodle/src/EmptyRestHandler.cpp" "/home/touchstone/ERF/symmetrical-octo-doodle/build/CMakeFiles/ERF.dir/src/EmptyRestHandler.cpp.o"
+  "/home/touchstone/ERF/symmetrical-octo-doodle/src/ForbiddenHandler.cpp" "/home/touchstone/ERF/symmetrical-octo-doodle/build/CMakeFiles/ERF.dir/src/ForbiddenHandler.cpp.o"
+  "/home/touchstone/ERF/symmetrical-octo-doodle/src/JSONUtil.cpp" "/home/touchstone/ERF/symmetrical-octo-doodle/build/CMakeFiles/ERF.dir/src/JSONUtil.cpp.o"
+  "/home/touchstone/ERF/symmetrical-octo-doodle/src/RestHandlerFactory.cpp" "/home/touchstone/ERF/symmetrical-octo-doodle/build/CMakeFiles/ERF.dir/src/RestHandlerFactory.cpp.o"
+  "/home/touchstone/ERF/symmetrical-octo-doodle/src/RestHandlerSelector.cpp" "/home/touchstone/ERF/symmetrical-octo-doodle/build/CMakeFiles/ERF.dir/src/RestHandlerSelector.cpp.o"
+  "/home/touchstone/ERF/symmetrical-octo-doodle/src/RestHandlerWithoutParamsProxy.cpp" "/home/touchstone/ERF/symmetrical-octo-doodle/build/CMakeFiles/ERF.dir/src/RestHandlerWithoutParamsProxy.cpp.o"
+  "/home/touchstone/ERF/symmetrical-octo-doodle/src/RestServer.cpp" "/home/touchstone/ERF/symmetrical-octo-doodle/build/CMakeFiles/ERF.dir/src/RestServer.cpp.o"
+  "/home/touchstone/ERF/symmetrical-octo-doodle/src/URIArgsParser.cpp" "/home/touchstone/ERF/symmetrical-octo-doodle/build/CMakeFiles/ERF.dir/src/URIArgsParser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
